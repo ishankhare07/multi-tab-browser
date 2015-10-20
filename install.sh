df1ce60d1f5cf334d87e7f01b99e0c6936b7747c
Ishan Khare <ishankhare07@gmail.com>
2015-10-20 17:08:38 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+if [ $EUID != "0" ]; then
+  gksu "$0 $@"
+else
+  echo "installing dependencies"
+  echo "======================="
+  sudo apt-get install python3-gi gir1.2-gtk-3.0 gtk+3.0 gir1.2-webkit-3.0
+  echo "installing webkit dependencies"
+  echo "=============================="
+  sudo apt-get install libwebkitgtk-3.0-0 python-webkit
+  echo "installing dependencies done"
+  echo "****************************"
+fi
+
+echo "Now run the browser by command: python3 run.py"
