99f6adf5cb8dffe4e9bd2cf8cef757ae3aadf23b
Ishan Khare <ishankhare07@gmail.com>
2015-10-20 17:30:10 +0530

    modified install script fixed run.py

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 if [ "$EUID" != "0" ]; then
-  exec gksu "$0 $@"
-else
-  echo "installing dependencies"
-  echo "======================="
-  sudo apt-get install python3-gi gir1.2-gtk-3.0 gtk+3.0 gir1.2-webkit-3.0
-  echo "installing webkit dependencies"
-  echo "=============================="
-  sudo apt-get install libwebkitgtk-3.0-0 python-webkit
-  echo "installing dependencies done"
-  echo "****************************"
+  echo "$0 $@"
+  sudo -v
 fi
+
+echo "installing dependencies"
+echo "======================="
+sudo apt-get install python3-gi gir1.2-gtk-3.0 gtk+3.0 gir1.2-webkit-3.0
+echo "installing webkit dependencies"
+echo "=============================="
+sudo apt-get install libwebkitgtk-3.0-0 python-webkit
+echo "installing dependencies done"
+echo "****************************"
 
 echo "Now run the browser by command: python3 run.py"
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+from gi.repository import Gtk
 import MainWindow
 
-window = MainWindow()
+window = MainWindow.MainWindow()
 window.show_all()
 Gtk.main()
